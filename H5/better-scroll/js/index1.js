bd78cd960c859bf4bbcf8c7ac8be5c3904c03846
Shilei Ma <604237073@qq.com>
2018-07-18 23:42:08 +0800

    移动端 better-scroll幻灯片制作

---
+++ H5/better-scroll/js/index1.js
@@ -0,0 +1,20 @@
+(function(){
+    let list = document.querySelector(".list");
+    let inner = "";
+
+    for (let index = 0; index < 50; index++) {
+        inner += "<li>这是第" + index + "</li>";   
+    }
+    list.innerHTML = inner;
+})();
+
+(function(){
+    let wrap = document.querySelector('.wrap');
+    let bscroll = new BScroll(wrap,{
+        /* startX: 50,
+        startY: 30, */
+        scrollX: false,
+        scrollY: true
+    });
+    
+})();
