5b931614e6d6a5d4373e59eda01b860d4eb1637b
Loading <shileima@hotmail.com>
2018-09-06 18:14:48 +0800

    co模块实践及fs promise写法

--- node/fs-promise.js
+++ node/fs-promise.js
@@ -1,3 +1,15 @@
 /**
  * Created by Loading on 2018/9/6.
  */
+
+const fs = require('fs');
+const util = require('util');
+
+util.promisify(fs.readFile)('./package.json','utf8')
+    .then(JSON.parse)  //.then(data=>JSON.parse(data))
+    .then(data => {
+        console.log(data.name)
+    })
+    .catch(err => {
+        console.log(err)
+    })
