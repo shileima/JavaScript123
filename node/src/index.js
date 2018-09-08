3ba66e9aa535b4de5c1f92144b1cdd43fc27a961
Shilei Ma <604237073@qq.com>
2018-09-08 13:12:22 +0800

    配置 babel 让node支持import 和 async

--- node/src/index.js
+++ node/src/index.js
@@ -11,3 +11,11 @@ promisify(readFile)(r(__dirname,'../package.json'))
     /* wfs(r(__dirname, './name.txt'),String(data.name),'utf8') */
     afs(r(__dirname, './name.txt'),String(data.main)+"\r",'utf8')
 })
+
+// 异步操作
+const readAsync = promisify(readFile);
+(async function getData(){
+    let data = await readAsync(r(__dirname, '../package.json'));
+    //data = JSON.parse(data)
+    console.log(JSON.parse(data).name)
+})()
