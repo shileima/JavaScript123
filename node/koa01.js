fe46d70c4b0ccd7793524649bf6773a12b7ab5e2
Shilei Ma <604237073@qq.com>
2018-09-23 00:04:39 +0800

    jsonp 获取 koa2接口数据

--- node/koa01.js
+++ node/koa01.js
@@ -13,35 +13,35 @@ Object.defineProperty(app.context, 'host', {
 });
 app.context.host = '120,120,120,127';
 Object.defineProperty(app.context, 'db', {
-    get: function(){
+    get: function() {
         return '这里是通过app.context给ctx增加属性和'
     },
 });
-app.use(async (ctx, next) => {
-    if(ctx.path === '/favicon.ico'){return;}
-    const start = Date.now();
-    await next();
-    const ms = Date.now() - start;
-    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
-// koa-logger
+app.use(async(ctx, next) => {
+        if (ctx.path === '/favicon.ico') { return; }
+        const start = Date.now();
+        await next();
+        const ms = Date.now() - start;
+        console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+    })
+    // koa-logger
 app.use(logger())
-// response
+    // response
 app.use(async ctx => {
-    if(ctx.request.url == '/favicon.ico'){
+    if (ctx.request.url == '/favicon.ico') {
         return;
     }
     ctx.body = ctx;
-    console.log('app.env:',app.env)
-    console.log('headers:',ctx.headers)   // 0,0,0,0
-    console.log('ctx:',ctx.db)     // 这里是通过app.context给ctx增加属性和方法
-    console.log('app:',app)
-    console.log('app.callback:',app.callback)
-    console.log('app.callback():',app.callback())
+    console.log('app.env:', app.env)
+    console.log('headers:', ctx.headers) // 0,0,0,0
+    console.log('ctx:', ctx.db) // 这里是通过app.context给ctx增加属性和方法
+    console.log('app:', app)
+    console.log('app.callback:', app.callback)
+    console.log('app.callback():', app.callback())
     app.keys = new KeyGrip(['im loading', 'i like coding'], 'sha256');
-    console.log('app.keys:',app.keys)
+    console.log('app.keys:', app.keys)
 });
 
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log('server running at port 3000')
 });
