47736ab9b8f130af5c1930921d1198e73746d26e
loading <604237073@qq.com>
2019-03-05 11:14:40 +0800

    手写expres之http服务

--- node/express/express.js
+++ node/express/express.js
@@ -1,16 +1,19 @@
 const http = require('http');
 const url = require('url');
-
 function createApplication(){
   let app = function(req,res){
     const {pathname} = url.parse(req.url,true);
     for(let i=0;i<app.routes.length;i++){
       let route = app.routes[i];
-      if( route.method == req.method.toLowerCase() && route.path == pathname ){
-          return route.handler(req.res)
+      if( (route.method == req.method.toLowerCase() 
+          || route.method == 'all') 
+          && (route.path == pathname || route.path == '*') 
+        ){
+        console.log(route.handler.toString());
+        return route.handler(req,res)
       }
     }
-    res.end(`Cannot ${req.method} ${pathname}`)
+    res.end(`Cannot ${req.method} ${pathname}\r\n`)
   }
   app.listen = function(){
     let server = http.createServer(app);
@@ -19,13 +22,21 @@ function createApplication(){
 
   // 建立数组保存路由规则 
   app.routes = []
-  app.get = function(path,handler){
+  http.METHODS.forEach(method => {
+    method = method.toLowerCase();
+    app[method] = function(path,handler){
+      app.routes.push({method, path, handler})
+    }
+  })
+
+  app.all = function(path,handler){
     app.routes.push({
-      method: 'get',
-      path,
+      method:'all', 
+      path, 
       handler
     })
   }
+
   return app;
 }
 module.exports = createApplication;
