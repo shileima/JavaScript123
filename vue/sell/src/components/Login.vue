1579bb912a8c724e9c99fb1104a1bea6748af904
Shilei Ma <604237073@qq.com>
2019-03-17 22:30:46 +0800

    vue cli experience

---
+++ vue/sell/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'Login Page'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
