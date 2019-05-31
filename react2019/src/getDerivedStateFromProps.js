544c6b2e0a333bf4770ac0daf3dca6b618a69dd9
loading <604237073@qq.com>
2019-05-31 18:13:54 +0800

    getDerivedPropsFromProps

---
+++ react2019/src/getDerivedStateFromProps.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class DerivedStateFromProps extends React.Component {
+    /* ES7 写法 */
+    state = {
+        num: 0
+    }
+    static getDerivedStateFromProps(props,state){
+      
+      console.log('props:',props)
+      console.log('state:',state)
+      return {
+        num:props.a
+      }
+    }
+
+    render(){
+      return ( <h4>getDerivedStateFromProps {this.state.num}</h4> )
+    }
+    
+}
+
+export default DerivedStateFromProps;
