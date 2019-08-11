32a207cc2acf072f8a7e2a2978f627d7ae502a65
Shilei Ma <604237073@qq.com>
2019-08-11 19:40:19 +0800

    cms-front project init and login

---
+++ react/cms-front/src/pages/login/constants.js
@@ -0,0 +1,34 @@
+export const addressOptions = [
+    {
+      value: 'zhejiang',
+      label: '浙江省',
+      children: [
+        {
+          value: 'hangzhou',
+          label: '杭州市',
+          children: [
+            {
+              value: 'xihu',
+              label: '西湖区',
+            },
+          ],
+        },
+      ],
+    },
+    {
+      value: 'jiangsu',
+      label: 'Jiangsu',
+      children: [
+        {
+          value: 'nanjing',
+          label: 'Nanjing',
+          children: [
+            {
+              value: 'zhonghuamen',
+              label: 'Zhong Hua Men',
+            },
+          ],
+        },
+      ],
+    },
+  ];
