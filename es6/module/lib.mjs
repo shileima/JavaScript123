220221ce81a1998db9535ee753a5b3eacc658776
mashilei <mashilei@meituan.com>
2021-01-18 16:26:06 +0800

    js module

---
+++ es6/module/lib.mjs
@@ -0,0 +1,11 @@
+/*
+ * @Description:
+ * @Date: 2021-01-18 16:17:10
+ * @LastEditors: mashilei@meituan.com
+ * @LastEditTime: 2021-01-18 16:19:26
+ * @FilePath: /JavaScript123/es6/module/lib.mjs
+ */
+export const repeat = (string) => `${string}${string}`;
+export function shout(string) {
+    return `${string.toUpperCase()}`;
+}
